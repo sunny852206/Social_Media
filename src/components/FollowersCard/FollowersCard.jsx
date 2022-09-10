edd09e84473e01d91133e8c7a783601c1035cb7c
Sunny Lau <sunnylau@Sunnys-Mac-mini.local>
2022-09-09 21:51:41 -0700

    follwer section done with dummy follwers data

--- src/components/FollowersCard/FollowersCard.jsx
+++ src/components/FollowersCard/FollowersCard.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 import "./FollowersCard.css";
+
+import { Followers } from "../../Data/FollowersData";
 const FollowersCard = () => {
   return (
     <div className="FollowersCard">
-      <h3> Who is following you</h3>
+      <h4> Who is following you</h4>
+      {Followers.map((follower, id) => {
+        return (
+          <div className="Followers">
+            <div>
+              <img
+                src={follower.img}
+                alt="follower profile picture"
+                className="FollowerImg"
+              />
+              <div className="name">
+                <span>{follower.name}</span>
+                <span>@{follower.username}</span>
+              </div>
+            </div>
+            <button className="button fc-button">Follow</button>
+          </div>
+        );
+      })}
     </div>
   );
 };

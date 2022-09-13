5a183f6f793fcad524a36bdc3d480212bfe768b1
Sunny Lau <sunnylau@Sunnys-Mac-mini.local>
2022-09-12 20:13:09 -0700

    user register post request implemented

--- server/Routes/AuthRoute.js
+++ server/Routes/AuthRoute.js
@@ -1,9 +1,13 @@
 import express from "express";
+import { registerUser } from "../Controllers/AuthController.js";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  res.send("Auth Route");
-});
+router.post("/register", registerUser);
+
+// no use
+// router.get("/", async (req, res) => {
+//   res.send("Auth Route");
+// });
 
 export default router;

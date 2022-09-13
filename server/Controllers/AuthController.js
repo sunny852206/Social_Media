4b07bacae0986c0ded4fbfd45d27cb17f6c07f22
Sunny Lau <sunnylau@Sunnys-Mac-mini.local>
2022-09-13 13:06:21 -0700

    hashing password for user for security purposes

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -1,16 +1,17 @@
 import UserModel from "../Models/userModel.js";
-// import bcrypt from "bcrypt";
+import bcrypt from "bcrypt";
 
 // Registering a new User
 export const registerUser = async (req, res) => {
   const { username, password, firstname, lastname } = req.body;
 
-  //   const salt = await bcrypt.genSalt(10);
-  //   const hashedPass = await bcrypt.hash(password, salt);
+  // generate Salt for password which need to encrypted
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassWord = await bcrypt.hash(password, salt);
 
   const newUser = new UserModel({
     username,
-    password,
+    password: hashedPassWord,
     firstname,
     lastname,
   });

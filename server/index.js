0a21eddb43cc04d20e2440bdfcb87db4529fd24d
Sunny Lau <sunnylau@Sunnys-Mac-mini.local>
2022-09-12 19:30:36 -0700

    storing credientials seperately

--- server/index.js
+++ server/index.js
@@ -1,20 +1,29 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
 // Middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-// dotenv.config();
+// Storing configuration seperately
+dotenv.config();
 
 mongoose
-  .connect(
-    "mongodb+srv://SocialMedia:SocialMedia@cluster0.jh3pu6g.mongodb.net/?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
+  .connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(process.env.PORT, () =>
+      console.log(`Listening at ${process.env.PORT}`)
+    )
   )
-  .then(() => app.listen(5000, () => console.log("Listening")));
+  .catch((error) => {
+    console.log(error);
+  });
 
 /*
 mongoose
